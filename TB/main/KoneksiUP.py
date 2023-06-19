72491692f1bc3850352202c375c619acbb792eb8
Luthfi HM <123862774+vysanss@users.noreply.github.com>
2023-06-19 19:25:33 +0700

    lanjut bang

--- TB/main/KoneksiUP.py
+++ TB/main/KoneksiUP.py
@@ -25,3 +25,36 @@ def tampilkan_kontak(kontak):
             print(f"Nama: {nama}")
             print(f"Nomor: {nomor}")
             print("=======================")
+def cari_kontak(kontak, nama):
+    print("Data kontak:")
+    for data in kontak:
+        if data['nama'] == nama:
+            nama = data['nama']
+            nomor = data['nomor']
+            print(f"Nama: {nama}")
+            print(f"Nomor: {nomor}")
+            print("=======================")
+    
+def insertion_sort(kontak):
+    for i in range(1, len(kontak)):
+        current = kontak[i]
+        j = i - 1
+        while j >= 0 and kontak[j]['nama'] > current['nama']:
+            kontak[j + 1] = kontak[j]
+            j -= 1
+        kontak[j + 1] = current
+
+def blokir_kontak(kontak, daftar_blokir, nama):
+    for i in range(len(kontak)):
+        if kontak[i]['nama'] == nama:
+            kontak[i]['status'] = False
+            if kontak[i]['nama'] == nama and kontak[i]['status'] == False:
+                nama_kontak_blokir = {
+                    'nama': kontak[i]['nama'],
+                    'nomor': kontak[i]['nomor']
+                }
+                daftar_blokir.append(nama_kontak_blokir)
+                print("Kontak berhasil diblokir!")
+            else:
+                print("Nama tidak ditemukan!")
+    return None
